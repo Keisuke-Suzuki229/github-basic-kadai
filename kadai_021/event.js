707297a331cefd3096268d51f83e83d1785e4002
Keisuke-Suzuki229 <154766861+Keisuke-Suzuki229@users.noreply.github.com>
2024-01-27 14:32:50 +1300

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-const h2Text = document.getElementById('text');
-
-const PushBtn = document.getElementById('btn');
-
-PushBtn.addEventListener('click',() => {
-  setTimeout(() => {
-    h2Text.textContent = 'ボタンをクリックしました';
-  }, 2000);
-})
